76f52e4cd416cfad78d3f357d5ee3419d25e34fa
Delson Tan <delsonwtan@gmail.com>
2018-07-26 21:00:46 +0000

    Do small adjustments to ensure scripts execute in the right order, before installation of a deck of cards package

---
+++ public/scripts/goofspiel.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+
+
+    // THE FUN BEGINS
+    var canvas = document.getElementById("Goofspiel");
+    canvas.width = 1280;
+    canvas.height = 768;
+    var ctx = canvas.getContext("2d");
+
+    
+})
