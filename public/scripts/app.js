bb53ebc3b385a3d88eebe6b187a57d246432439e
Paul <paul.c.lo@gmail.com>
2018-07-27 03:16:37 +0000

    get gamestate to apps.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,18 +13,26 @@
 
 
 
-// POLLING.....
-// (function poll(){
-//   setTimeout(function(){
-//     $.ajax({
-//       method: "GET",
-//       url: "/api/users"
-//     }).done((users) => {
-//       for(user of users) {
-//         $("<div>").text(user.name).appendTo($("body"));
-//       };
-//       console.log("Refresh")
-//       poll();
-//     });;
-//  }, 1000);
-// })();
+var gameState;
+(function poll(){
+  setTimeout(function(){
+    $.ajax({
+      method: "GET",
+      url: "/gs"
+    }).done((gs) => {
+     
+      console.log(gs)
+      poll();
+    });;
+ }, 1000);
+})();
+
+
+// $.ajax({
+//   method: "POST",
+//   url: "/tweets",
+//   data: $(this).serialize()
+// })
+//   .done(function(newTweet) {
+//     loadTweets([newTweet]);
+//   })
