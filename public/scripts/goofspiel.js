d5fd25c1d6ed4337efb745c5a829ae0184c5c156
Delson Tan <delsonwtan@gmail.com>
2018-07-28 03:45:13 +0000

    Break code, go back in time

--- public/scripts/goofspiel.js
+++ public/scripts/goofspiel.js
@@ -1,81 +1,111 @@
 $(document).ready(function () {
-
-    var gameState;
-
-    // polling server for new gamestate
-    (function poll() {
-        setTimeout(function () {
-            $.ajax({
-                method: "GET",
-                url: "/gs"
-            }).done((gs) => {
-                // only update if timestamp is later
-                gameState = gs;
-                draw();
-                poll();
-            });;
-        }, 1000);
-    })();
-
-
-    function postGameState() {
-        //update timestamp here 
-        gameState["timestamp"] = new Date();
-        $.ajax({
-            method: "POST",
-            url: "/gs",
-            data: gameState
-        })
-            .done(function () {
-                console.log("GS sent")
-            })
-    }
-
-    var canvas = document.getElementById("Goofspiel");
+    console.log(thisUser);
+    console.log(gameID);
     // Canvas dimensions
+    var canvas = document.getElementById("Goofspiel");
     canvas.width = 1600;
     canvas.height = 900;
     canvasTop = canvas.offsetTop;
     canvasLeft = canvas.offsetLeft;
     var ctx = canvas.getContext("2d");
 
-    var cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    var prizeValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    // Card specifications
+    var cardNames = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    var cardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     var playingCard = {
         width: 80,
         height: 120,
         backColor: "#CB4335",
         frontColor: "#FFFFFF"
     }
+
+    var playerCards = cardNames;
+    // Properties extracted from gamestate
+    var gameState = {};
+    var turnHistory = [];
+    var turn = 0;
+    var prizeCard = {};
+    var player1 = "";
+    var player2 = "";
+    var thePlayer = "";
+    var playerSuit = "Diamond";
+    var opponentSuit = "Club";
+    var prizeSuit = "Heart";
+    
+    var playerPlayed = {};
+
+
+    // Card objects for collision detection
     var playerHand = [];
     var opponentHand = [];
-    var playerPlayedCard = {};
-    var opponentPlayedCard = {};
-    var currentPrize = {};
-    var turnState = 0;
+
+    // polling server for new gamestate
+
+    setInterval(function poll() {
+        $.ajax({
+            method: "GET",
+            url: "/gs/" + (gameID).toString()
+        }).done((gs) => {
+            // {turnHistory: [], player1: string, player2: string}
+            console.log(gs);
+            gameState = gs;
+            turnHistory = gs.turnHistory;
+            turn = turnHistory.length;
+            prizeCard = turnHistory[turn - 1].prizeCard;
+            player1 = gs.player1;
+            player2 = gs.player2;
+            if (thisUser === player1) {
+                thePlayer = player1;
+            } else if (thisUser === player2) {
+                thePlayer = player2;
+            }
+            playerPlayed = turnHistory[turn - 1][thePlayer];
+            playerHand = getHand(turnHistory);
+
+        })
+    }, 1000);
+
+
+
+    // Turnstates - 
+    // 0: Initial setup of the game, only rendered once in the beginning
+    // 1: Prize card shown on play area, player allowed to make a move
+    // 2: Player made a move, opponent has not. Waiting...
+    // 3: Opponent made a move, player has not. Player allowed to make a move
+    // 4: Both players made their move. Resolution phase: No input allowed. Scores shown
+    // 5: Winning player places all 3 cards on his side of the table
+    // 6: Player Won
+    // 7: Player Lost
+    // 8: Tie
 
 
     canvas.addEventListener('click', function (event) {
         var mouseX = event.pageX - canvasLeft;
         var mouseY = event.pageY - canvasTop;
-
-        // Collision detection for player cards
+        console.log(playerHand);
         playerHand.forEach(function (card) {
-            if (turnstate === 1 && (mouseX > card.left && mouseX < card.left + playingCard.width)
-                && (mouseY > card.top && mouseY < card.top + playingCard.height)) {
-                //alert(`You clicked your ${cardValues[card.value - 1]}`);
-                playerPlayedCard = card;
-                if (turnState === 3) {
-                    turnState = 4;
-                } else {
-                    turnState = 2;
-                }
+            console.log(playerPlayed);
+            if (// mouseclick collision detection
+                ((mouseX > card.left && mouseX < card.left + playingCard.width)
+                    && (mouseY > card.top && mouseY < card.top + playingCard.height))
+                && // move validation, does not pass if move is illegal
+                !playerPlayed) {
+                //alert(`You clicked your ${cardNames[card.value - 1]}`);
+                playerCards.splice(playerCards.indexOf(card.name), 1);
+                //console.log(playerCards);
+                $.ajax({
+                    method: "POST",
+                    url: "/gs/",
+                    data: { name: cardName(card.name), suit: playerSuit } // {name: __, suit: ___}
+                }).done(function () {
+                    console.log(`Played card:, ${cardName(card.name)}, suit: ${playerSuit}`)
+                })
             }
         })
     })
 
     // Renders a card on canvas. Specify inner color and value if card is face up
-    function renderPlayingCard(xpos, ypos, innerColor, value) {
+    function renderPlayingCard(xpos, ypos, innerColor, name) {
         ctx.beginPath();
         ctx.rect(xpos, ypos, playingCard.width, playingCard.height);
         ctx.fillStyle = playingCard.backColor;
@@ -94,116 +124,181 @@ $(document).ready(function () {
             ctx.beginPath();
             ctx.font = "16px Arial";
             ctx.fillStyle = "#000000";
-            ctx.fillText(value, xpos + 20, ypos + 30);
+            ctx.fillText(name, xpos + 20, ypos + 30);
             ctx.closePath();
         }
     }
 
-    function renderPlayerHand() {
+    // Accepts an array of cardNames representing player cards and renders them in a row
+    function renderPlayerHand(playerCards) {
         var offsetX = 20;
         var y = canvas.height - playingCard.height - 20;
         playerhand = [];
-
-        for (var i = 0; i < cardValues.length; i++) {
-            var cardObj = {};
-            cardObj.value = prizeValues[i];
-            cardObj.left = offsetX;
-            cardObj.top = y;
-            cardObj.name = cardValues[i];
-            if (cardObj.name !== playerPlayedCard.name) {
-                playerHand.push(cardObj);
-                renderPlayingCard(cardObj.left, cardObj.top, playingCard.frontColor, cardValues[i]);
-                offsetX = offsetX + playingCard.width + 5;
+        for (var i = 0; i < cardNames.length; i++) {
+            for (var j = 0; j < playerCards.length; j++) {
+                if (cardNames[i] === playerCards[j]) {
+                    var cardObj = {};
+                    cardObj.name = cardNames[i];
+                    cardObj.value = cardValues[i];
+                    cardObj.left = offsetX;
+                    cardObj.top = y;
+                    playerHand.push(cardObj);
+                    renderPlayingCard(cardObj.left, cardObj.top, playingCard.frontColor, cardObj.name);
+                    offsetX = offsetX + playingCard.width + 5;
+                }
             }
         }
     }
 
-    function renderOpponentHand() {
+    // Accepts a number and renders that number of cards face down in a row
+    function renderOpponentHand(n) {
         var offSetX = 20;
         var y = 20;
         opponentHand = [];
-
-        for (var i = 0; i < cardValues.length; i++) {
+        for (var i = 0; i < n; i++) {
             var cardObj = {};
-            cardObj.value = prizeValues[i];
             cardObj.left = offSetX;
             cardObj.top = y;
-            cardObj.name = cardValues[i];
-            if (cardObj.name !== opponentPlayedCard.name) {
-                opponentHand.push(cardObj);
-                renderPlayingCard(cardObj.left, cardObj.top);
-                offSetX = offSetX + playingCard.width + 5;
-            }
+            opponentHand.push(cardObj);
+            renderPlayingCard(cardObj.left, cardObj.top);
+            offSetX = offSetX + playingCard.width + 5;
         }
     }
 
-    function renderPrizeDeck() {
-        renderPlayingCard((canvas.width - playingCard.width - 20), canvas.height / 2);
-    }
 
-    // renders the top card of the prize card array in game state on the play area
-    function renderPrizeCard() {
-        valueOfCard(gameState._prizeCards[0])
-        renderPlayingCard(canvas.width / 2, canvas.height / 2, playingCard.frontColor, valueOfCard(gameState._prizeCards[0]));
-    }
-
-    function renderPlayerPlayed() {
-        if (playerPlayedCard) {
-            renderPlayingCard(canvas.width / 2 - playingCard.width - 20, canvas.height / 2, playingCard.frontColor, playerPlayedCard.name);
+    // TODO: accepts a number and renders that number of cards face down stacked up
+    function renderPrizeDeck(n) {
+        var pixelsFromRightEdge = 30;
+        var offset = pixelsFromRightEdge / 15;
+        for (var i = 0; i < n; i++) {
+            renderPlayingCard((canvas.width - playingCard.width - pixelsFromRightEdge + offset), canvas.height / 2 + offset);
+            offset = offset + pixelsFromRightEdge / 15;
         }
     }
 
-    function renderOpponentPlayed() {
-        if (opponentPlayedCard) {
-            renderPlayingCard(canvas.width / 2 + playingCard.width + 20, canvas.height / 2, playingCard.frontColor, opponentPlayedCard.name);
+    // Accepts a string that should be the card's name and renders it on the center of the screen
+    function renderPrizeCard(str) {
+        cardName = cardInitial(str);
+        renderPlayingCard(canvas.width / 2, canvas.height / 2, playingCard.frontColor, cardName);
+
+    }
+
+    // Accepts a card object and renders it to the left and offset down to a card on the center of the screen face up
+    function renderPlayerPlayed(card) {
+        if (card) {
+            var offsetX = 20;
+            var offsetY = 20;
+            cardName = cardInitial(card);
+            renderPlayingCard(canvas.width / 2 - playingCard.width - offsetX, canvas.height / 2 + offsetY, playingCard.frontColor, cardName);
         }
     }
 
-    function drawPrizeCard() {
-        console.log(gameState._prizeCards[0]);
+    // Accepts a card object and renders it to the right and offset up to a card on the center of the screen face down
+    function renderOpponentPlayed(card) {
+        if (card) {
+            var offsetX = 20;
+            var offsetY = 20;
+            renderPlayingCard(canvas.width / 2 + playingCard.width + offsetX, canvas.height / 2 - offsetY, playingCard.frontColor, card.name);
+        }
     }
+
+    // TODO: function which accepts opponent card object and renders it on the center of the screen face up
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        renderPlayerHand();
-        renderOpponentHand();
-        renderPrizeDeck();
-        renderPrizeCard();
+        // TODO: render scoreboard
+        renderPlayerHand(playerCards);//gameState.turnHistory, thisPlayer));
+        renderOpponentHand(13);
+        renderPrizeDeck(13);
+        // TODO: render player winnings pile
+        // TODO: render opponent winnings pile
+        renderPrizeCard("Ace"); //gamestate.turnHistory[0].prizeCard
+        renderPrizePile(gameState.prizePile);
         renderPlayerPlayed();
         renderOpponentPlayed();
-        if (turnState === 2) {
-            // remove when redundant
-            opponentMove();
-        }
-        if (turnState === 4) {
-            setTimeout(function () { turnstate = 5 }, 5000);
-        }
-    }
-    turnstate = 1;
-    console.log(turnstate);
-    console.log(opponentPlayedCard == true)
 
-    // Simulates an opponent moving, remove when obsolete
-    function opponentMove() {
-        opponentPlayedCard = opponentHand[0];
-        if (turnState === 2) {
-            turnState = 4;
-        } else {
-            turnState = 3;
-        }
+        // animate cards moving to the winning side's winnings pile
+        // CHECK IF PLAYER WON/LOST, change to state 6/7
+
+        // TODO: highlight player green in scoreboard
+        // and highlight opponent red
+        // TODO: show big text: "YOU WON"
+
+        // TODO: highlight player red in scoreboard
+        // and highlight opponent green
+        // TODO: show big text: "YOU LOST"
+
+        requestAnimationFrame(draw);
     }
 
-    function valueOfCard(gsCard) {
-        result = gsCard.name;
-        if ((gsCard.name) === "Ace") {
+    function thePlayer(user) {
+        if (!gameState) {
+            return undefined;
+        }
+        if (user === gameState.player1) {
+            return "player1";
+        } else if (user === gameState.player2) {
+            return "player2";
+        }
+    }
+
+    function cardInitial(cardN) {
+        var result = cardN;
+        if (cardN === "Ace") {
             result = "A";
-        } else if ((gsCard.name) === "Jack") {
+        } else if (cardN === "Jack") {
             result = "J";
-        } else if ((gsCard.name) === "Queen") {
+        } else if (cardN === "Queen") {
             result = "Q";
-        } else if ((gsCard.name) === "King") {
+        } else if (cardN === "King") {
             result = "K";
         }
         return result;
     }
+
+    function cardName(cardI) {
+        result = cardI.toString();
+        if ((cardI) === "A") {
+            result = "Ace";
+        } else if ((cardI) === "J") {
+            result = "Jack";
+        } else if ((cardI) === "Q") {
+            result = "Queen";
+        } else if ((cardI) === "K") {
+            result = "King";
+        }
+        return result;
+    }
+    // uses gameState's turnHistory array to create an array of cards with properies required for collision detection
+    function getHand(history) {
+        // var offsetX = 20;
+        // var y = canvas.height - playingCard.height - 20;
+        // playerhand = [];
+        // for (var i = 0; i < cardNames.length; i++) {
+        //     for (var j = 0; j < playerCards.length; j++) {
+        //         if (cardNames[i] === playerCards[j]) {
+        //             var cardObj = {};
+        //             cardObj.name = cardNames[i];
+        //             cardObj.value = cardValues[i];
+        //             cardObj.left = offsetX;
+        //             cardObj.top = y;
+        //             playerHand.push(cardObj);
+        //             offsetX = offsetX + playingCard.width + 5;
+        //         }
+        //     }
+        // }
+    }
+
+    function getOpponentHand(n) {
+        if (n) {
+            return 13 - n;
+        }
+        else return 13;
+    }
+
+    //TODO: function to produce array of strings representing player card names from gamestate
+
+    //TODO: function to produce a number representing number of opponent cards from gamestate
+
+    draw();
 })
--- server.js
+++ server.js
@@ -2,18 +2,18 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
+const bodyParser = require("body-parser");
+const sass = require("node-sass-middleware");
+const app = express();
 const cookieSession = require("cookie-session");
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig = require("./knexfile");
+const knex = require("knex")(knexConfig[ENV]);
+const morgan = require('morgan');
+const knexLogger = require('knex-logger');
 
 // Serializer
 // var s = require("serialijse");
@@ -70,17 +70,28 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/games/:id", (req, res) => {
-  var localVars= {gameID: req.params.id,
-                  username: req.session.user_id};
-  
-  res.redirect("games/" + , localVars);
+  const gameURL = (GameState.findMatchGameIDFor(req.session.user_id)).toString();
+  // check gamestate: 0 players, 1 player waiting, or full
+
+  // 2: full, try generating another random string and trying again?
+  // 1: join game
+  // 0: generate new id and insert into database if making new game
+  res.redirect(`/games/${gameURL}`);
 });
+
+app.get("/games/:id", (req, res) => {
+  var localVars = {
+    gameID: req.params.id,
+    username: req.session.user_id
+  };
+  res.render("games_show", localVars)
+})
 
 app.post("/login", (req, res) => {
   const user = req.body.username;
   req.session.user_id = user;
   DataHelpers.fetchUser(user, (err, result) => {
-    if(err) { throw err };
+    if (err) { throw err };
     GameState.matchParticipant(user);
     res.redirect("/");
   });
@@ -103,7 +114,7 @@ app.post("/gs/", (req, res) => {
 // Game State Class to capture state context *****************
 class GameState {
   constructor() {
-    this.game_id = GameState.all().length+1;
+    this.game_id = GameState.all().length + 1;
     this.initialize();
     GameState.all().push(this);
   };
@@ -113,11 +124,12 @@ class GameState {
     this.turnsP2 = [];
     this.prizeCards = Card.randomizeCardsFor(Card.getHearts());
   }
-  addParticipant(userName) { 
-    if(this._player1 === undefined) {
+  addParticipant(userName) {
+    if (this._player1 === undefined) {
       this._player1 = userName
-    } else { 
-      this._player2 = userName }
+    } else {
+      this._player2 = userName
+    }
     return this
   };
   needParticipants() { return this.participants.length < 2 };
@@ -125,11 +137,11 @@ class GameState {
 
   get prizeCards() { this._prizeCards };
   set prizeCards(cards) { this._prizeCards = cards };
-  get turnsP1() { this._turnsP1};
+  get turnsP1() { this._turnsP1 };
   set turnsP1(cards) { this._turnsP1 = cards };
-  get turnsP2() { this._turnsP2};
+  get turnsP2() { this._turnsP2 };
   set turnsP2(cards) { this._turnsP2 = cards };
-  participants()  { return [this._player1, this.player2] };
+  participants() { return [this._player1, this.player2] };
 
   static findMatchGameIDFor(userName) { return this.matchParticipant(userName).game_id }
   static matchParticipant(userName) {
@@ -140,22 +152,28 @@ class GameState {
   }
   userAndTurnHistories() {
     let turns = Math.min(this._turnsP1.length, this._turnsP2.length, 12) + 1;
-    return {  turnHistory: [...Array(turns).keys()]
-        .map(i => { return {  "player1": this._turnsP1.length <= i ? this._turnsP1[i] : undefined,
-                              "player2": this._turnsP2.length <= i ? this._turnsP2[i] : undefined,
-                              "prizeCard": this._prizeCards[i]}}),
-              player1: this._player1,
-              player2: this._player2
+    return {
+      turnHistory: [...Array(turns).keys()]
+        .map(i => {
+          return {
+            "player1": this._turnsP1.length <= i ? this._turnsP1[i] : undefined,
+            "player2": this._turnsP2.length <= i ? this._turnsP2[i] : undefined,
+            "prizeCard": this._prizeCards[i]
+          }
+        }),
+      player1: this._player1,
+      player2: this._player2
     }
   }
   static allParticipatingFor(username) {
     this.all().filter(each => each.hasParticipant(userName))
   }
-  static findWith(game_id) { return this.all().find(each => each.game_id = game_id) };
-  static all() { 
-      if (this._all === undefined)
-        this._all = [];
-      return this._all }
+  static findWith(game_id) { return this.all().find(each => each.game_id === Number(game_id))};
+  static all() {
+    if (this._all === undefined)
+      this._all = [];
+    return this._all
+  }
 }
 
 // Card class represents deck of cards *****************
@@ -171,13 +189,13 @@ class Card {
     do {
       let num = Math.floor(Math.random() * cards.length);
       results.push(cards.splice(num, 1)[0]);
-    } while(cards.length > 0)
+    } while (cards.length > 0)
     return results;
   }
   static initializeCards() {
     var result = [];
     ['Spade', 'Heart', 'Club', 'Diamond'].forEach(mySuit => {
-      ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'].forEach (myName => {
+      ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'].forEach(myName => {
         result.push(new Card(myName, mySuit))
       })
     });
@@ -206,7 +224,7 @@ DataHelpers.getUsers((x, result) => console.log(result));
 
 function fetchUser(username) {
   DataHelpers.fetchUser(username, (err, result) => {
-    if(err) { throw err };
+    if (err) { throw err };
     return result;
   });
 };
